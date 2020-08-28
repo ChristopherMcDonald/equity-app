ab0ec836356f8b306432f348b11e672a9d3021eb
Christopher McDonald <mail@christophermcdonald.me>
2020-08-27 22:55:46 -0400

    add range

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -8,21 +8,12 @@
             v-for="(player, i) in players"
             v-bind:key="i"
             :color="isPrimary(i)"
-            @click="selectPlayer(i)">
-            {{ player.name }}
-          </v-btn>
-          <v-btn
-            v-if="players.length < 9"
-            color="success"
-            @click="addPlayer">
-            +
-          </v-btn>
+            @click="selectPlayer(i)"
+          >{{ player.name }}</v-btn>
+          <v-btn v-if="players.length < 9" color="success" @click="addPlayer">+</v-btn>
         </v-row>
         <v-row no-gutters class="left-item">
-          <v-text-field
-            v-model="players[selected].name"
-            label="Player's Name">
-          </v-text-field>
+          <v-text-field v-model="players[selected].name" label="Player's Name"></v-text-field>
           <v-row v-for="(row, j) in allHands()" v-bind:key="j" no-gutters>
             <v-col class="button" v-for="col in row" v-bind:key="col">
               <v-btn
@@ -32,7 +23,7 @@
               >{{ col }}</v-btn>
             </v-col>
           </v-row>
-          <v-row class="left-item">
+          <v-row class="full-width left-item">
             <v-btn
               class="warning pad-right"
               :disabled="players.length === 1"
@@ -40,31 +31,32 @@
             >Delete Player</v-btn>
             <v-btn class="error" @click="clearRange">Clear Range</v-btn>
           </v-row>
+          <v-row class="left-item">
+            <v-subheader class="pl-0">Range (%)</v-subheader>
+            <v-slider
+              v-model="players[selected].rangePercentage"
+              thumb-label
+            ></v-slider>
+          </v-row>
         </v-row>
       </v-col>
       <v-col class="calc">
         <h3>Range Calculator</h3>
         <div v-for="(player, i) in players" v-bind:key="i">
           <h3>{{player.name}}</h3>
-          <v-progress-linear
-            v-model="player.winRate"
-            :color="getColor(player.winRate)"
-            height="25"
-          >
+          <v-progress-linear v-model="player.winRate" :color="getColor(player.winRate)" height="25">
             <template v-slot="{ value }">
               <strong>{{ Math.ceil(value) }}%</strong>
             </template>
           </v-progress-linear>
-          <br>
+          <br />
         </div>
-        <br>
+        <br />
         <v-btn
           color="primary"
           @click="getEquity"
           :disabled="players.some((player) => player.range.length === 0)"
-        >
-          Calculate Equity
-        </v-btn>
+        >Calculate Equity</v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -73,6 +65,7 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import { Watch } from 'vue-property-decorator';
 import { CardGroup, OddsCalculator } from 'poker-odds-calculator';
 import handMixin from '../utils/hands';
 
@@ -80,19 +73,26 @@ import handMixin from '../utils/hands';
   mixins: [handMixin],
 })
 export default class Main extends Vue {
-  players: { name: string; range: string[]; winRate: number }[] = [{
-    name: 'Chris',
-    range: ['AA', 'KK'],
-    winRate: 0.0,
-  }, {
-    name: 'Cameron',
-    range: ['AA', 'KK', 'AKo', 'AKs'],
-    winRate: 0.0,
-  }, {
-    name: 'Forest',
-    range: ['AA', 'AKo', 'AQo', 'AJo', 'ATo', 'A9o', 'A8o', 'A7o', 'A6o', 'A5o', 'A4o', 'A3o', 'A2o', 'AKs', 'KK', 'KQo', 'KJo', 'KTo', 'K9o', 'K8o', 'K7o', 'K6o', 'K5o', 'K4o', 'K3o', 'K2o', 'AQs', 'KQs', 'QQ', 'QJo', 'QTo', 'Q9o', 'Q8o', 'Q7o', 'Q6o', 'Q5o', 'Q4o', 'Q3o', 'Q2o', 'AJs', 'KJs', 'QJs', 'JJ', 'JTo', 'J9o', 'J8o', 'J7o', 'J6o', 'J5o', 'J4o', 'J3o', 'J2o', 'ATs', 'KTs', 'QTs', 'JTs', 'TT', 'T9o', 'T8o', 'T7o', 'T6o', 'T5o', 'T4o', 'T3o', 'T2o', 'A9s', 'K9s', 'Q9s', 'J9s', 'T9s', '99', '98o', '97o', '96o', '95o', '94o', '93o', '92o', 'A8s', 'K8s', 'Q8s', 'J8s', 'T8s', '98s', '88', '87o', '86o', '85o', '84o', '83o', '82o', 'A7s', 'K7s', 'Q7s', 'J7s', 'T7s', '97s', '87s', '77', '76o', '75o', '74o', '73o', '72o', 'A6s', 'K6s', 'Q6s', 'J6s', 'T6s', '96s', '86s', '76s', '66', '65o', '64o', '63o', '62o', 'A5s', 'K5s', 'Q5s', 'J5s', 'T5s', '95s', '85s', '75s', '65s', '55', '54o', '53o', '52o', 'A4s', 'K4s', 'Q4s', 'J4s', 'T4s', '94s', '84s', '74s', '64s', '54s', '44', '43o', '42o', 'A3s', 'K3s', 'Q3s', 'J3s', 'T3s', '93s', '83s', '73s', '63s', '53s', '43s', '33', '32o', 'A2s', 'K2s', 'Q2s', 'J2s', 'T2s', '92s', '82s', '72s', '62s', '52s', '42s', '32s', '22'],
-    winRate: 0.0,
-  }];
+  players: { name: string; range: string[]; winRate: number; rangePercentage: number }[] = [
+    {
+      name: 'Chris',
+      range: ['AA', 'KK'],
+      winRate: 0.0,
+      rangePercentage: 1.0,
+    },
+    {
+      name: 'Cameron',
+      range: ['AA', 'KK', 'AKo', 'AKs'],
+      winRate: 0.0,
+      rangePercentage: 2.0,
+    },
+    {
+      name: 'Forest',
+      range: ['AA', 'AKo', 'AQo', 'AJo', 'ATo', 'A9o', 'A8o', 'A7o', 'A6o', 'A5o', 'A4o', 'A3o', 'A2o', 'AKs', 'KK', 'KQo', 'KJo', 'KTo', 'K9o', 'K8o', 'K7o', 'K6o', 'K5o', 'K4o', 'K3o', 'K2o', 'AQs', 'KQs', 'QQ', 'QJo', 'QTo', 'Q9o', 'Q8o', 'Q7o', 'Q6o', 'Q5o', 'Q4o', 'Q3o', 'Q2o', 'AJs', 'KJs', 'QJs', 'JJ', 'JTo', 'J9o', 'J8o', 'J7o', 'J6o', 'J5o', 'J4o', 'J3o', 'J2o', 'ATs', 'KTs', 'QTs', 'JTs', 'TT', 'T9o', 'T8o', 'T7o', 'T6o', 'T5o', 'T4o', 'T3o', 'T2o', 'A9s', 'K9s', 'Q9s', 'J9s', 'T9s', '99', '98o', '97o', '96o', '95o', '94o', '93o', '92o', 'A8s', 'K8s', 'Q8s', 'J8s', 'T8s', '98s', '88', '87o', '86o', '85o', '84o', '83o', '82o', 'A7s', 'K7s', 'Q7s', 'J7s', 'T7s', '97s', '87s', '77', '76o', '75o', '74o', '73o', '72o', 'A6s', 'K6s', 'Q6s', 'J6s', 'T6s', '96s', '86s', '76s', '66', '65o', '64o', '63o', '62o', 'A5s', 'K5s', 'Q5s', 'J5s', 'T5s', '95s', '85s', '75s', '65s', '55', '54o', '53o', '52o', 'A4s', 'K4s', 'Q4s', 'J4s', 'T4s', '94s', '84s', '74s', '64s', '54s', '44', '43o', '42o', 'A3s', 'K3s', 'Q3s', 'J3s', 'T3s', '93s', '83s', '73s', '63s', '53s', '43s', '33', '32o', 'A2s', 'K2s', 'Q2s', 'J2s', 'T2s', '92s', '82s', '72s', '62s', '52s', '42s', '32s', '22'],
+      winRate: 0.0,
+      rangePercentage: 100.0,
+    },
+  ];
 
   selected = 0;
 
@@ -100,11 +100,25 @@ export default class Main extends Vue {
     this.getEquity();
   }
 
+  @Watch('selectedRange')
+  onSelectedRangeChange(a: number) {
+    this.players[this.selected].range = (
+      handMixin
+        .allHandsRanked
+        .slice(0, Math.ceil((handMixin.allHandsRanked.length - 1) * (a / 100)))
+    );
+  }
+
+  get selectedRange() {
+    return this.players[this.selected].rangePercentage;
+  }
+
   getColor(percent: number) {
     const all = this.players.map((a) => a.winRate);
     if (Math.max(...all) === percent) {
       return 'green';
-    } if (Math.min(...all) === percent) {
+    }
+    if (Math.min(...all) === percent) {
       return 'red';
     }
 
@@ -112,7 +126,12 @@ export default class Main extends Vue {
   }
 
   addPlayer() {
-    this.players.push({ name: `Player ${this.players.length + 1}`, range: [], winRate: 0.0 });
+    this.players.push({
+      name: `Player ${this.players.length + 1}`,
+      range: [],
+      winRate: 0.0,
+      rangePercentage: 0.0,
+    });
   }
 
   isPrimary(i: number) {
@@ -165,9 +184,9 @@ export default class Main extends Vue {
       const takenHands: string[] = [];
       const takenCards: string[] = [];
       holdings.forEach((holding) => {
-        const acceptableHands = holding.range.filter((h) => (
-          ![h.substring(0, 2), h.substring(2, 4)].some((j) => takenCards.includes(j))
-        ));
+        const acceptableHands = holding.range.filter(
+          (h) => ![h.substring(0, 2), h.substring(2, 4)].some((j) => takenCards.includes(j)),
+        );
 
         const randomHand = acceptableHands[Math.floor(Math.random() * acceptableHands.length)];
         takenHands.push(randomHand);
@@ -182,13 +201,16 @@ export default class Main extends Vue {
         undefined,
         100,
       ).equities.forEach((equity, i) => {
-        holdings[i].equities.push(equity.getEquity() + equity.getTiePercentage());
+        holdings[i].equities.push(
+          equity.getEquity() + equity.getTiePercentage(),
+        );
       });
     }
 
     holdings.forEach((holding, i) => {
-      this.players[i].winRate = holding.equities.reduce((a, b) => a + b, 0)
-      / holding.equities.length;
+      this.players[i].winRate = (
+        holding.equities.reduce((a, b) => a + b, 0) / holding.equities.length
+      );
     });
   }
 }
@@ -200,11 +222,11 @@ export default class Main extends Vue {
 }
 
 .left-item {
-    margin: 3% 0;
+  margin: 3% 0;
 }
 
 .pad-right {
-    margin: 0 3% 0 0;
+  margin: 0 3% 0 0;
 }
 
 .pad-bottom {
@@ -213,5 +235,9 @@ export default class Main extends Vue {
 
 .calc {
   margin: 1%;
+}
+
+.full-width {
+  width: 100%;
 }
 </style>
--- src/utils/hands.ts
+++ src/utils/hands.ts
@@ -1,5 +1,35 @@
 const ranks = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'];
 
+const allHandsFlat = [
+  'AA', 'AKo', 'AQo', 'AJo', 'ATo', 'A9o', 'A8o', 'A7o', 'A6o', 'A5o', 'A4o', 'A3o', 'A2o', 'AKs', 'KK',
+  'KQo', 'KJo', 'KTo', 'K9o', 'K8o', 'K7o', 'K6o', 'K5o', 'K4o', 'K3o', 'K2o', 'AQs', 'KQs', 'QQ', 'QJo',
+  'QTo', 'Q9o', 'Q8o', 'Q7o', 'Q6o', 'Q5o', 'Q4o', 'Q3o', 'Q2o', 'AJs', 'KJs', 'QJs', 'JJ', 'JTo', 'J9o',
+  'J8o', 'J7o', 'J6o', 'J5o', 'J4o', 'J3o', 'J2o', 'ATs', 'KTs', 'QTs', 'JTs', 'TT', 'T9o', 'T8o', 'T7o',
+  'T6o', 'T5o', 'T4o', 'T3o', 'T2o', 'A9s', 'K9s', 'Q9s', 'J9s', 'T9s', '99', '98o', '97o', '96o', '95o',
+  '94o', '93o', '92o', 'A8s', 'K8s', 'Q8s', 'J8s', 'T8s', '98s', '88', '87o', '86o', '85o', '84o', '83o',
+  '82o', 'A7s', 'K7s', 'Q7s', 'J7s', 'T7s', '97s', '87s', '77', '76o', '75o', '74o', '73o', '72o', 'A6s',
+  'K6s', 'Q6s', 'J6s', 'T6s', '96s', '86s', '76s', '66', '65o', '64o', '63o', '62o', 'A5s', 'K5s', 'Q5s',
+  'J5s', 'T5s', '95s', '85s', '75s', '65s', '55', '54o', '53o', '52o', 'A4s', 'K4s', 'Q4s', 'J4s', 'T4s',
+  '94s', '84s', '74s', '64s', '54s', '44', '43o', '42o', 'A3s', 'K3s', 'Q3s', 'J3s', 'T3s', '93s', '83s',
+  '73s', '63s', '53s', '43s', '33', '32o', 'A2s', 'K2s', 'Q2s', 'J2s', 'T2s', '92s', '82s', '72s', '62s',
+  '52s', '42s', '32s', '22',
+];
+
+const allHandsRanked = [
+  'AA', 'KK', 'QQ', 'JJ', 'TT', '99', '88', 'AKs', 'AQs', '77', 'AJs', 'AKo', 'ATs', 'AQo', 'AJo', 'A9s',
+  'KQs', 'KJs', 'ATo', '66', 'A8s', 'A7s', 'KTs', 'A6s', 'A5s', 'KQo', 'KJo', 'A9o', 'A4s', 'K9s', 'QJs',
+  'QTs', 'KTo', 'A8o', '55', 'A3s', 'K8s', 'A7o', 'A5o', 'A2s', 'K7s', 'Q9s', 'JTs', 'QJo', 'QTo', 'K9o',
+  'A6o', 'A4o', 'K6s', 'K5s', 'Q8s', 'K8o', 'A3o', '44', 'K4s', 'J9s', 'JTo', 'Q9o', 'K7o', 'A2o', 'K3s',
+  'Q7s', 'J8s', 'T9s', 'K6o', 'K2s', 'Q6s', 'Q5s', 'J9o', 'Q8o', 'K5o', '33', 'Q4s', 'J7s', 'T8s', 'Q7o',
+  'K4o', 'K3o', 'Q3s', 'J6s', 'T7s', '98s', 'T9o', 'J8o', 'Q6o', 'K2o', 'Q2s', 'J5s', 'T8o', 'J7o', 'Q5o',
+  'J4s', 'T6s', '97s', 'Q4o', '22', 'J2s', 'J3s', '87s', '96s', '98o', 'T7o', 'J6o', 'Q3o', 'T4s', 'T5s',
+  '86s', 'J5o', 'Q2o', 'T3s', '95s', '76s', '97o', 'T6o', 'J4o', 'J3o', 'T2s', '85s', '87o', '96o', 'T5o',
+  'J2o', '94s', '93s', '75s', '65s', '86o', 'T4o', '92s', '84s', '74s', '76o', '95o', 'T3o', '64s', '54s',
+  '85o', 'T2o', '83s', '82s', '73s', '53s', '75o', '65o', '94o', '93o', '63s', '43s', '84o', '92o', '72s',
+  '52s', '74o', '64o', '54o', '62s', '42s', '83o', '82o', '73o', '63o', '53o', '32s', '43o', '72o', '62o',
+  '52o', '42o', '32o',
+];
+
 const allHands = () => {
   const all: string[][] = [];
   for (let i = 0; i < ranks.length; i += 1) {
@@ -41,6 +71,8 @@ const transformRange = (range: string[]) => range.reduce<string[]>((arr, r) => {
 }, []);
 
 export default {
+  allHandsFlat,
+  allHandsRanked,
   methods: {
     allHands,
     transformRange,
